393f56864122e42b7f21a8f4c1769790e94658c6
Kana Otawara <kana.otawara@nextbeat.net>
2019-09-10 14:57:20 +0900

    ABC139-E

--- ABC/139/e.cpp
+++ ABC/139/e.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+typedef pair<int, int> P;
+
+void toId(int x, int y) {
+
+}
+
+void check() {
+    if ()
+}
+
+deque<P> dq;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  int a[n][n - 1];
+  for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - 1; j++) {
+          cin >> a[i][j - 1];
+      }
+  }
+
+}
 
