37bb2d59049f27c12c5ed1c9e5eadf74e9e6218e
Kana Otawara <shimmer22.0204@gmail.com>
2019-10-03 01:45:45 +0900

    Qiita AtCoder版蟻本　全探索

--- AtCoder/ABC/104/c.py
+++ AtCoder/ABC/104/c.py
@@ -12,7 +12,7 @@ for bit in range(2 ** d):
     score = 0
 
     # ボーナス分の足し込み
-    for i in range(d - 1, 0, -1):
+    for i in range(d - 1, -1, -1):
         if bit >> i & 1 == 1:
             cnt += p[i]
             score += p[i] * (i + 1) * 100 + c[i]
@@ -20,14 +20,18 @@ for bit in range(2 ** d):
     if score >= g:
         ans = min(ans, cnt)
     else:
-        for i in range(d - 1, 0, -1):
-            if bit >> i & 1 == 0:
-                num = min(p[i] - 1, int(math.ceil((g - score) / (i + 1) * 100)))
-                cnt += num
-                score += num * (i + 1) * 100
+        for i in range(d - 1, -1, -1):
+            if bit >> i & 1 == 1:
+                continue
             
-            if score >= g:
-                ans = min(ans, cnt)
-                break
+            for j in range(p[i]):
+                if score >= g:
+                    break
+
+                cnt += 1
+                score += (i + 1) * 100
+            
+        if score >= g:
+            ans = min(ans, cnt)
 
 print(ans)
